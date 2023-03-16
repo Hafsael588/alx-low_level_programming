83d391c4292dfc73b266b4b319930cf4bd914e7c
Hafsael588 <elmerzouki.hafsa.1998@gmail.com>
2023-03-16 15:08:29 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - A program that prints a line with puts function
- * return 0;
+ * Return : 0 (Succes)
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
